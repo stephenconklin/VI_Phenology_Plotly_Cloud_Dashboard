6085c8b6ef296deaee44ea3e2b114603b6a8a62a
Stephen Conklin <stephenconklin@gmail.com>
2026-04-21 14:34:20 -0600

    Disable debug mode and update dashboard screenshot

--- app.py
+++ app.py
@@ -1222,4 +1222,4 @@ if _SHAPEFILE_LIST:
 # ---------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
